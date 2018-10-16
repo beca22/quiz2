2e6296bafef38775ce6212aff7787c1dff9c6b9f
alumno Secundaria <al3201@cedi.local>
2018-10-16 08:08:53 -0500

    Ya terminado el proyecto, esta listo para subirlo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+bocina=BocinaPage;
+tv = TvPage;
+smartphone= SmartphonePage;
+lampara = LamparaPage;
   constructor(public navCtrl: NavController) {
 
   }
-
+clickBocina(){
+this.navCtrl.push(this.bocina);
+}
+clickTV(){
+  this.navCtrl.push(this.tv);
+}
+clickSmartphone(){
+  this.navCtrl.push(this.smartphone)
+}
+clickLampara(){
+  this.navCtrl.push(this.lampara)
+}
 }
